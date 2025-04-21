b9a2502d28e8933b20f744fba67e3594fa410343
ARVIND_VERMA <arvindverma630635@gmail.com>
2025-04-21 18:22:05 +0530

    Update complete booking dialog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.app.ace_taxi_v2"
         minSdk = 24
         targetSdk = 34
-        versionCode = 42
-        versionName = "5.1"
+        versionCode = 43
+        versionName = "5.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
