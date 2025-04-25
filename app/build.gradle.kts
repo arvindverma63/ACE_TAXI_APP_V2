1553759e295be89b6bf8b3f51d3a1b59752200a0
ARVIND_VERMA <arvindverma630635@gmail.com>
2025-04-25 18:42:55 +0530

    update background theme (v 6.1)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.app.ace_taxi_v2"
         minSdk = 24
         targetSdk = 34
-        versionCode = 51
-        versionName = "6.0"
+        versionCode = 52
+        versionName = "6.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
