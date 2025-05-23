647b6061314cf9bc4eb193937cd8c4dc50fd1f6d
ARVIND_VERMA <arvindverma630635@gmail.com>
2025-05-23 18:03:28 +0530

    Add logger (logger laravel)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.app.ace_taxi_v2"
         minSdk = 24
         targetSdk = 34
-        versionCode = 85
-        versionName = "9.5"
+        versionCode = 86
+        versionName = "9.6"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
